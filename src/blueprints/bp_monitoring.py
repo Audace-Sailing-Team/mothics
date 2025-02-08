85f8fcef83b88762aba57b14e29e600a8db77a58
iricci <iagor@mailbox.org>
2025-02-08 17:57:31 +0100

    Add toggles for upcoming bokeh server

--- src/blueprints/bp_monitoring.py
+++ src/blueprints/bp_monitoring.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request, Response, current_app
+from bokeh.embed import server_document
 from ..bokeh_plots import create_bokeh_plots
 from ..helpers import compute_status
 
@@ -7,8 +8,16 @@ monitor_bp = Blueprint('monitor', __name__)
 
 @monitor_bp.route("/")
 def index():
-    database = current_app.config['GETTERS']['database']()
-    script, div = create_bokeh_plots(database)
+    # Select plot source
+    mode = current_app.config['PLOT_MODE']
+    if mode == 'real-time':
+        # Use server_document to generate the script that will embed the Bokeh app.
+        script = server_document(current_app.config['PLOT_REALTIME_URL'])
+        div = ""
+    else:
+        database = current_app.config['GETTERS']['database']()
+        script, div = create_bokeh_plots(database)
+        
     auto_refresh = current_app.config['AUTO_REFRESH_TABLE']
     return render_template("index.html", script=script, div=div, auto_refresh=auto_refresh)
 
