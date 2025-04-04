5a45588c8d15da3b1a45338d64365fb1bca6f357
iricci <dummy.iricci@dummy.com>
2025-04-04 12:52:51 +0200

    Fix last

--- mothics/__init__.py
+++ mothics/__init__.py
@@ -3,6 +3,6 @@ Mothics is a package for acquiring, aggregating, and analyzing data
 collected from remote sensors (*Remote Units*) for sailing on moth
 boats.
 
-Mothics is developed by the Electronics department of `Audace Sailing
-Team <https://audace.units.it/>`_, University of Trieste.
+Mothics is developed by the Electronics department of [Audace Sailing
+Team](https://audace.units.it/), University of Trieste.
 """
